073e439918599daa4a97b9ee0346d585c9cb7bdd
huong295 <lethanhhuong2905@gmail.com>
2023-09-19 19:37:08 +0700

    update

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+  
+}
+
+const App = (props: Props) => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1> 
+    </div>
+  )
+}
+
+export default App
