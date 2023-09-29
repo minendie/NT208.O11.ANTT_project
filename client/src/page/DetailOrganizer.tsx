eb1610f9a754c759dfecbdc18be404730c86a2ec
uyenbhku <uyen.bhku@gmail.com>
2023-09-29 13:37:11 +0700

    add organizer's profile (no CSS)

--- client/src/page/DetailOrganizer.tsx
+++ client/src/page/DetailOrganizer.tsx
@@ -1,13 +1,55 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import OrganizerProfileForm from '../component/form/OrganizerProfileForm'
+import { UserOutlined } from "@ant-design/icons";
+import { Avatar } from "antd";
 
-interface Props {
-    
+interface ProfileProps {
+    currentUserID: number,
+    targetUserID: number,
 }
 
-const DetailOrganizer = (props: Props) => {
+interface Organizer {
+    avatarSrc: string,
+    organizerName: string,
+    email: string,
+    phoneNumber: string,
+    description: string,
+    address: string,
+    fbLink?: string,
+    websiteLink?: string,
+    linkedInLink?: string,
+}
+
+
+const DetailOrganizer = (props: ProfileProps) => {
+    const [organizer, setOrganizer] = useState<Organizer|null>(null);
+    const [canEdit, setCanEdit] = useState(false);
+
+    useEffect(() => {
+        // fetch target organizer's data from the database based on their ID
+        if (props.currentUserID === props.targetUserID) {
+            setCanEdit(true);
+            // fetch organizer's data with password
+        } else {
+            // fetch organizer's data without password
+        }
+    })
+
     return (
         <div>
-        detail organizer    
+            <div className='profile-header'>
+                <div className="profile-thumbnail"></div>
+                {
+                    !organizer 
+                    ? <Avatar className="profile-avatar" shape="circle" icon={<UserOutlined />}></Avatar>
+                    : <img alt="User's avatar" src={organizer?.avatarSrc} /> 
+                }
+                <h2>{organizer? organizer.organizerName : 'Test Username'}</h2>
+            </div>
+            <OrganizerProfileForm 
+                canEdit={canEdit}
+                organizer={organizer}
+            />
         </div>
     )
 }
