220281338a8145b865ff2ed7fb9c5e4963bc32d7
hnuynnhi <uyennhi27072003@gmail.com>
2023-10-05 19:35:15 +0700

    UI Detail Campaign

---
+++ client/src/page/DetailCampaign.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import TabsPage from '../component/card/TabsPage/TabsPage'
+interface Props {
+    
+}
+
+const DetailCampaign = () => {
+    return (
+        <div>
+           <TabsPage/>
+        </div>
+    )
+}
+
+export default DetailCampaign
