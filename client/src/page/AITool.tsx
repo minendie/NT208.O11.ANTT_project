c9c2929ff0bd8dcf803a32b433103458be27cdb4
uyenbhku <uyen.bhku@gmail.com>
2023-10-09 12:32:29 +0700

    Fix image display AITool (add headers) - last change 2

--- client/src/page/AITool.tsx
+++ client/src/page/AITool.tsx
@@ -3,9 +3,7 @@ import '../component/AITool/styles.css';
 import { Component } from 'react';
 import DropZone from '../component/AITool/DropZone';
 import AIToolResultRight from '../component/AITool/AIToolResultRight';
-
-
-const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT
+import AIToolLogo from '../assets/AI_Tool_logo.svg'
 
 
 // declare props for AITool component
@@ -75,7 +73,7 @@ export class AITool extends Component<AIToolProps, AIToolState> {
                 <>
                   <div className='AITool-content'>
                     <div className='AITool-header'>
-                      <img className='AITool-header-logo' src={`${API_ENDPOINT}/image/AI_Tool_logo.svg`} />
+                      <img className='AITool-header-logo' src={AIToolLogo} cross-origin="anonymous"/>
                       <h1 className='AITool-header-title'>AI-powered waste sorting tool</h1>
                     </div>
                     <p className='AITool-desc'>An AI-powered waste sorting tool utilizes advanced algorithms and machine learning to automate and optimize the process of waste classification.</p>
